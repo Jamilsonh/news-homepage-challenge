b009041ce1e4d10a8415a67ef51b2739a4d3dfe5
jamilsonh <2jamilson@gmail.com>
2024-01-02 19:57:18 -0300

    feat: Creating header component

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { Container, RightMenu } from './styles';
+
+export function HomePage() {
+  return (
+    <Container>
+      <Image
+        src='logo.svg'
+        width={65}
+        height={40}
+        alt='Picture of the author'
+      />
+
+      <RightMenu>
+        <h2>Home</h2>
+        <h2>New</h2>
+        <h2>Popular</h2>
+        <h2>Trending</h2>
+        <h2>Categories</h2>
+      </RightMenu>
+    </Container>
+  );
+}
