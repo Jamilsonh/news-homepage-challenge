d40d799be67681c3180fb836d77b46b807470d19
jamilsonh <2jamilson@gmail.com>
2024-01-05 21:56:13 -0300

    refactor:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,8 @@
 
 import Image from 'next/image';
 import styles from './page.module.css';
-import { HomePage } from '@/pages/HomePage';
+import { HomePage } from './pages/HomePage';
 
 export default function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 }
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import { BodyContainer } from './styles';
-import { Header } from '@/Components/Header';
-import RightBody from '@/Components/Body/RightBody';
-import LeftBody from '@/Components/Body/LeftBody';
-import Footer from '@/Components/Footer';
+import { Header } from '@/app/Components/Header';
+import LeftBody from '@/app/Components/Body/LeftBody';
+import RightBody from '@/app/Components/Body/RightBody';
+import Footer from '@/app/Components/Footer';
 
 export function HomePage() {
   return (
